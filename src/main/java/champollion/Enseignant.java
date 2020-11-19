e04fd836ff34e4fb0c8aa52eecec1cc509491082
lisa <lisa.estebe@gmail.com>
2020-11-19 11:32:32 +0100

    heuresPlanifiees et enSousService

--- src/main/java/champollion/Enseignant.java
+++ src/main/java/champollion/Enseignant.java
@@ -7,51 +7,54 @@ import java.util.List;
 public class Enseignant extends Personne {
 
     // TODO : rajouter les autres méthodes présentes dans le diagramme UML
-
     private final List<ServicePrevu> myServicePrevu = new LinkedList<>();
     private final List<Intervention> myInterventions = new LinkedList<>();
-    
+
     public Enseignant(String nom, String email) {
         super(nom, email);
     }
 
     /**
-     * Calcule le nombre total d'heures prévues pour cet enseignant en "heures équivalent TD" Pour le calcul : 1 heure
-     * de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut 0,75h
-     * "équivalent TD"
+     * Calcule le nombre total d'heures prévues pour cet enseignant en "heures
+     * équivalent TD" Pour le calcul : 1 heure de cours magistral vaut 1,5 h
+     * "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure de TP vaut
+     * 0,75h "équivalent TD"
      *
-     * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
+     * @return le nombre total d'heures "équivalent TD" prévues pour cet
+     * enseignant, arrondi à l'entier le plus proche
      *
      */
     public int heuresPrevues() {
         // TODO: Implémenter cette méthode
-        float compteur=0;
-        for (ServicePrevu s : myServicePrevu){
-            compteur += s.getCM()*1.5;
+        float compteur = 0;
+        for (ServicePrevu s : myServicePrevu) {
+            compteur += s.getCM() * 1.5;
             compteur += s.getTD();
-            compteur += s.getTP()*0.75;
+            compteur += s.getTP() * 0.75;
         }
         return round(compteur);
     }
 
     /**
-     * Calcule le nombre total d'heures prévues pour cet enseignant dans l'UE spécifiée en "heures équivalent TD" Pour
-     * le calcul : 1 heure de cours magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent TD" 1 heure
-     * de TP vaut 0,75h "équivalent TD"
+     * Calcule le nombre total d'heures prévues pour cet enseignant dans l'UE
+     * spécifiée en "heures équivalent TD" Pour le calcul : 1 heure de cours
+     * magistral vaut 1,5 h "équivalent TD" 1 heure de TD vaut 1h "équivalent
+     * TD" 1 heure de TP vaut 0,75h "équivalent TD"
      *
      * @param ue l'UE concernée
-     * @return le nombre total d'heures "équivalent TD" prévues pour cet enseignant, arrondi à l'entier le plus proche
+     * @return le nombre total d'heures "équivalent TD" prévues pour cet
+     * enseignant, arrondi à l'entier le plus proche
      *
      */
     public int heuresPrevuesPourUE(UE ue) {
         // TODO: Implémenter cette méthode
-        float compteur=0;
-        for (ServicePrevu s : myServicePrevu){
-            if (s.getUe()==ue){
-                compteur += s.getCM()*1.5;
+        float compteur = 0;
+        for (ServicePrevu s : myServicePrevu) {
+            if (s.getUe() == ue) {
+                compteur += s.getCM() * 1.5;
                 compteur += s.getTD();
-                compteur += s.getTP()*0.75;
-            }  
+                compteur += s.getTP() * 0.75;
+            }
         }
         return round(compteur);
     }
@@ -67,11 +70,38 @@ public class Enseignant extends Personne {
     public void ajouteEnseignement(UE ue, int volumeCM, int volumeTD, int volumeTP) {
         // TODO: Implémenter cette méthode
         myServicePrevu.add(new ServicePrevu(ue, volumeCM, volumeTD, volumeTP));
-        
+
     }
-    
-    public void ajouteIntervention(Intervention i){
+
+    public void ajouteIntervention(Intervention i) {
         myInterventions.add(i);
     }
 
+    public int heuresPlanifiees() {
+        float compteur = 0;
+        for (Intervention i : myInterventions) {
+            if (null == i.getType()) {
+                compteur += i.getDuree();
+            } else {
+                switch (i.getType()) {
+                    case CM:
+                        compteur += (i.getDuree() * 1.5);
+                        break;
+                    case TP:
+                        compteur += (i.getDuree() * 0.75);
+                        break;
+                    default:
+                        compteur += i.getDuree();
+                        break;
+                }
+            }
+        }
+        return round(compteur);
+    }
+    
+    public boolean enSousService(){
+        return this.heuresPlanifiees()<this.heuresPrevues();
+    }
 }
+
+
